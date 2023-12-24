bb3df89566df793accd5170346a95bbb2748245d
Chris Tartamella <chris@tartamella.me>
2023-12-24 11:14:59 -0700

    sadf

--- database/dbo/Scripts/populate_length_unit.PostDeployment.sql
+++ database/dbo/Scripts/populate_length_unit.PostDeployment.sql
@@ -1,35 +1,35 @@
-PRINT 'Populating Lookup Table [dbo].[length_unit]';
+-- PRINT 'Populating Lookup Table [dbo].[length_unit]';
 
-GO;
+-- GO;
 
-MERGE INTO [dbo].[length_unit] as t
-    USING (
-        VALUES(1,'Minutes')
-             ,(2,'Hours')
-             ,(3,'Laps')
-             ,(4,'Miles')
-             ,(5,'Kilometers')
-    ) s ([id],[name])
-        ON t.[id] = s.[id]
-WHEN MATCHED
-    THEN 
-        UPDATE
-            SET [name] = s.[name]
-WHEN NOT MATCHED BY TARGET
-    AND [s].[id] IS NOT NULL 
-    THEN
-        INSERT 
-        (
-            [id]
-            ,[name]
-        )
-        VALUES 
-        (
-            [id]
-            ,[name]
-        )
-WHEN NOT MATCHED BY SOURCE 
-   THEN 
-       DELETE;
+-- MERGE INTO [dbo].[length_unit] as t
+--     USING (
+--         VALUES(1,'Minutes')
+--              ,(2,'Hours')
+--              ,(3,'Laps')
+--              ,(4,'Miles')
+--              ,(5,'Kilometers')
+--     ) s ([id],[name])
+--         ON t.[id] = s.[id]
+-- WHEN MATCHED
+--     THEN 
+--         UPDATE
+--             SET [name] = s.[name]
+-- WHEN NOT MATCHED BY TARGET
+--     AND [s].[id] IS NOT NULL 
+--     THEN
+--         INSERT 
+--         (
+--             [id]
+--             ,[name]
+--         )
+--         VALUES 
+--         (
+--             [id]
+--             ,[name]
+--         )
+-- WHEN NOT MATCHED BY SOURCE 
+--    THEN 
+--        DELETE;
 
-GO
+-- GO
